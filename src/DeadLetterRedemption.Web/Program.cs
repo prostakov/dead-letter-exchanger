1caed64973f4c22f67d3971461938f574b937c5d
Serhii Prostakov <serhii.prostrakov@gmail.com>
2021-10-30 16:59:26 +0300

    Global renaming refactoring

--- src/DeadLetterRedemption.Web/Program.cs
+++ src/DeadLetterRedemption.Web/Program.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace DeadLetterRedemption
+namespace DeadLetterRedemption.Web
 {
     public class Program
     {
