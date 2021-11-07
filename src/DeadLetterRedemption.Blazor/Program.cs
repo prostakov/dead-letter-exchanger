44b28e2d699743cc4207942dd8b27d656eef9362
Serhii Prostakov <serhii.prostrakov@gmail.com>
2021-11-07 18:47:41 +0200

    Add http client

--- src/DeadLetterRedemption.Blazor/Program.cs
+++ src/DeadLetterRedemption.Blazor/Program.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using DeadLetterRedemption.Common;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,12 +9,22 @@ namespace DeadLetterRedemption.Blazor
 {
     public class Program
     {
+        private const string ServerApi = nameof(ServerApi);
+        
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
+            
+            // Add http client
+            builder.Services.AddHttpClient(ServerApi, 
+                client => client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
+            builder.Services.AddScoped(
+                sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient(ServerApi));
 
+            // Add signalr client
             builder.Services.AddScoped<AppClient>();
+            
             builder.Services.AddScoped<AppStateManager>();
             
             // TODO: Authorization
