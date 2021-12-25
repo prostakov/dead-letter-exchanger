9b1bed89027e3cea82bf0b12935dc236ea3d4d56
Serhii Prostakov <serhii.prostrakov@gmail.com>
2021-12-25 16:48:41 +0200

    Add integration tests project

---
+++ src/Tests/DeadLetterRedemption.Integration.Tests/EventsTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace DeadLetterRedemption.Integration.Tests
+{
+    [TestFixture, Explicit]
+    public class EventsTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
